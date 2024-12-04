d65209d0f409c7f35ced8a33830b57cbf295c327
gretchypr <gretchen.bonilla@upr.edu>
2024-12-04 17:46:07 -0400

    Added counting frequency with a map

--- frequency_count.cpp
+++ frequency_count.cpp
@@ -5,6 +5,24 @@
 
 using namespace std;
 
-void showWordFrequency(string sentece) {
-    string cleanStr = sentence
+void showLetterFrequency(vector<char> letters) {
+    unordered_map<char, int> frequencies;
+    for(auto c: letters) {
+        if(frequencies.find(c) == frequencies.end())
+            frequencies[c] = 1;
+        else
+            frequencies[c] = frequencies[c] + 1;
+    }
+
+    cout << "Frequencies of all the letters: " << endl;
+    for (const auto pair : frequencies) {
+        cout << pair.first << ": " << pair.second << endl;
+    }
+}
+
+int main() {
+    vector<char> letters = {'a', 'b', 'c', 'a', 'b', 'd', 'e', 'f', 'g', 'a',
+                            'b', 'a', 'a', 'c', 'd', 'e', 'a', 'b', 'w', 'f', 'g'};
+    showLetterFrequency(letters);
+    return 0;
 }
